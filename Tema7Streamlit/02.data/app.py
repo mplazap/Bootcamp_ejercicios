3f9fbe74b1f2d99182bde1ae023afd4ba23ae874
Marino Plaza <123264649+mplazap@users.noreply.github.com>
2025-02-24 19:33:36 +0100

    Ejercicios

--- Tema7Streamlit/02.data/app.py
+++ Tema7Streamlit/02.data/app.py
@@ -1,2 +1,28 @@
 import streamlit as st
+import seaborn as sns
 
+# Desactivo el certificado SSL de manera temporal para que no me de fallos
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context 
+
+st.title('Streamlit API reference Data Elements')
+
+st.header('Mostrar un DataFrame de Pandas')
+
+# Cacheamos el dataframe para evitar recargarlo todo el rato
+@st.cache_data
+def load_tips_data():   
+    return sns.load_dataset('tips')
+
+df = load_tips_data()
+
+st.dataframe(df, use_container_width=True)
+
+st.header('Mostrar un DataFrame editable de Pandas')
+
+df_edited = st.data_editor(df, use_container_width=True)
+
+st.write('Este metodo``st.data_editor``permite modificar celdas, podemos guardar los cambios del dataframe')
+st.write('Resultado del **dataframe editado**')
+
+st.dataframe(df_edited, use_container_width=True)
