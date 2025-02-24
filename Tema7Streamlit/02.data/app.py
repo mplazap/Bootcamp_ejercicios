964706d70ac70f5e15484dd457b946225d19042d
Marino Plaza <123264649+mplazap@users.noreply.github.com>
2025-02-24 20:52:21 +0100

    Ejercicios

--- Tema7Streamlit/02.data/app.py
+++ Tema7Streamlit/02.data/app.py
@@ -1,28 +1,63 @@
 import streamlit as st
 import seaborn as sns
+import requests
 
-# Desactivo el certificado SSL de manera temporal para que no me de fallos
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context 
-
-st.title('Streamlit API reference Data Elements')
-
-st.header('Mostrar un DataFrame de Pandas')
-
-# Cacheamos el dataframe para evitar recargarlo todo el rato
+# Decorador para cachear el resultado de la función, es decir, cachear el dataframe para evitar recargarlo todo el rato
 @st.cache_data
-def load_tips_data():   
+def load_tips_data():
     return sns.load_dataset('tips')
 
-df = load_tips_data()
+# @st.cache_data
+def load_json_data():
+    url = 'https://fakestoreapi.com/products/1'
+    return requests.get(url).json()
 
+st.title('Streamlit API reference Data elements')
+
+#
+# 1. MOSTRAR DATAFRAME
+#
+st.header('1. DataFrame NO editable de Pandas con st.dataframe')
+df = load_tips_data()
 st.dataframe(df, use_container_width=True)
 
-st.header('Mostrar un DataFrame editable de Pandas')
 
+#
+# 2. DATAFRAME EDITABLE
+#
+st.header('2. DataFrame editable de Pandas con st.data_editor')
 df_edited = st.data_editor(df, use_container_width=True)
 
-st.write('Este metodo``st.data_editor``permite modificar celdas, podemos guardar los cambios del dataframe')
-st.write('Resultado del **dataframe editado**')
 
+#
+# 3. COMPROBAR MODIFICACIONES SOBRE DATASET EDITADO
+# 
+st.header('3. DataFrame NO editable de Pandas con st.dataframe para comprobar datos editados.')
+st.write('''
+Este método ``st.data_editor`` permite modificar celdas, podemos guardar el dataframe resultante. 
+Ejemplo de **dataframe editado** resultante:
+''')
 st.dataframe(df_edited, use_container_width=True)
+
+#
+# 4. TABLAS ESTÁTICAS
+#
+st.header('4. Tabla estática con st.table')
+st.write('Tabla estática sin funcionalidades interactivas.')
+st.table(df.head())
+
+#
+# 5. JSON
+#
+st.header('5. Mostrar datos JSON')
+st.json(load_json_data())
+
+#
+# 6. Metrics
+# 
+st.header('Métricas')
+st.metric('tu rendimiento', value=10, delta=2)
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
