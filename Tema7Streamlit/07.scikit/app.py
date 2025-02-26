1dc16dfb1ec7960d60930e25a4fa3e9a7f183fa0
Marino Plaza <123264649+mplazap@users.noreply.github.com>
2025-02-26 19:09:02 +0100

    Ejercicios

---
+++ Tema7Streamlit/07.scikit/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+
+@st.cache_resource
+def load_scikit_model():
+    return joblib.load('pipeline.joblib')
+
+st.title('Regresion Tip')
+
+model = load_scikit_model()
+
+st.header('Introduce los datos para la predicción')
+
+
